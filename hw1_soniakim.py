42077e3b3b040b83806aca3215f45736b9f68028
Sonia <sekim912@gmail.com>
2020-10-10 21:53:49 -0400

    Added hw1_soniakim.py

---
+++ hw1_soniakim.py
@@ -0,0 +1,24 @@
+#Homework Assignment for HW1
+
+#Question 1
+def compute_emi(rate, N, PV, FV):
+   while(1):
+       try:
+           PV = int(input("Please enter your principal amount borrowed, in integers: "))
+           FV = int(input("Please enter the future value you want to attain after the last payment is made, in integers: "))
+       except:
+           print("Not an integer! Try again")
+       else: print(PV+FV/((1+0.04)^20))*(0.04*((1+0.04)^20)/((1+0.04)^20)-1)
+
+    print (PV+FV/((1+rate)^N))*(rate*((1+rate)^N)/((1+rate)^N)-1)
+
+return (PV+FV/((1+0.04)^20))*(0.04*((1+0.04)^20)/((1+0.04)^20)-1)
+
+#Question 2
+import os
+os.chdir("/Users/sk4490/Documents/computing_bs/homework/hw1")
+
+myfile = open('question2.txt')
+for line in myfile:
+    print(line, end=" ")
+myfile.close()
